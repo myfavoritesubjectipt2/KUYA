ea31dd842cea10bc2105221e6d430024a9ff3a30
unknown <ashleytorio@gmail.com>
2024-09-10 12:27:02 +0800

    Divide

--- calculator.java
+++ calculator.java
@@ -14,3 +14,10 @@ public class Calculator {
         public static int multiply(int a, int b) {
             return a * b;
         }
+
+        public static double divide(int a, int b) {
+            if (b == 0) {
+                throw new ArithmeticException("Cannot divide by zero");
+            }
+            return (double) a / b;
+        }
